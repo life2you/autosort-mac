69a244f27ec11a536b69d442255cf066bb228859
life2you <henry@henglin.com>
2026-05-07 18:44:18 +0800

    Update Homebrew formula for v0.1.0

--- homebrew/autosort-mac.rb
+++ homebrew/autosort-mac.rb
@@ -2,7 +2,7 @@ class AutosortMac < Formula
   desc "Automatically organize Desktop and Downloads files on macOS"
   homepage "https://github.com/life2you/autosort-mac"
   url "https://github.com/life2you/autosort-mac/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "SHA256_PLACEHOLDER"
+  sha256 "96772fd3737003e31e779faff940744b28e4beb0405c189c427fc7f3d602c3b4"
   license "MIT"
 
   depends_on "rust" => :build
